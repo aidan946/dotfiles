63a8122efb1855f569945ec514345c18381f229a
Aidan <aidanstevens90@gmail.com>
2026-04-27 18:33:29 +1000

    Add transparency

--- nix/ghostty.nix
+++ nix/ghostty.nix
@@ -2,11 +2,15 @@
   programs.ghostty = {
     enable = true;
     package = null;
+    systemd.enable = false;
     settings = {
       font-family = "Maple Mono";
       font-style = "Regular";
       font-size = 16;
       font-feature = "cv31,cv32,cv33,cv34,cv35,cv36,cv37,cv38,cv66,ss03,ss10,ss11";
+
+      background-opacity = 0.9;
+      background-blur-radius = 32;
 
       theme = "Catppuccin Mocha";
       window-padding-x = 8;
--- nvim/plugin/01-catppuccin.lua
+++ nvim/plugin/01-catppuccin.lua
@@ -1,2 +1,3 @@
 vim.pack.add { { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin' } }
+require('catppuccin').setup { transparent_background = true }
 vim.cmd.colorscheme 'catppuccin-nvim'
